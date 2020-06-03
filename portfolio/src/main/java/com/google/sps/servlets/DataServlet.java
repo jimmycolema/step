26c63c9882a1e53a7c90e4e884d863a4772fa4d4
Jimmy <jimmycolema@google.com>
2020-06-03 16:15:11 +0000

    Use Datastore to store comments permanently

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -14,6 +14,9 @@
 
 package com.google.sps.servlets;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
 import com.google.gson.Gson;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -49,9 +52,13 @@ public class DataServlet extends HttpServlet {
       comment = comment.toLowerCase();
     }
 
-    addCommentToForum(comment);
+    Entity taskEntity = new Entity("Comment");
+    taskEntity.setProperty("comment", comment);
+
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    datastore.put(taskEntity);
+
     response.sendRedirect("/index.html");
-    return;
   }
 
   private String convertToJson(ArrayList<String> messages) {
