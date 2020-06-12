935a6e78b0b24dfddcd57f7ebe7af5ed4cb7b0aa
Jimmy <jimmycolema@google.com>
2020-06-12 17:47:58 +0000

    Passes all tests except for nestedEvents

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -19,6 +19,7 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public final class FindMeetingQuery {
 
@@ -26,21 +27,49 @@ public final class FindMeetingQuery {
     // Collect parameters of meeting request
     Collection<String> attendees = request.getAttendees();
     Collection<String> optionalAttendees = request.getAttendees();
-    long duration = request.getDuration();
+    int duration = (int) request.getDuration();
 
-    Collection<TimeRange> viableTimes = new ArrayList<TimeRange>();
-    Collection<TimeRange> reservedTimes = new ArrayList<TimeRange>();
+    List<TimeRange> viableTimes = new ArrayList<>();
+    List<TimeRange> reservedTimes = new ArrayList<>();
 
-    // Case: Duration is longer than a day
-    if (duration > TimeRange.WHOLE_DAY.duration()) {
-      return viableTimes;
+    // List of pre-existing events sorted by starting time
+    events.stream()
+          .sorted((e1, e2) -> e1.getWhen().start() - e2.getWhen().start())
+          .filter (e -> !Collections.disjoint(e.getAttendees(), attendees))
+          .forEach(e -> {
+            reservedTimes.add(e.getWhen());
+          });
+
+    viableTimes = calculateViableTimes(reservedTimes, duration);
+    return viableTimes;
+  }
+
+  public List<TimeRange> calculateViableTimes(List<TimeRange> reservedTimes, long duration) {
+    List<TimeRange> viableTimes = new ArrayList<>();
+    int potentialStart = TimeRange.START_OF_DAY;
+
+    for (int i = 0; i < reservedTimes.size(); i++) {
+      int reservedTimeStart = reservedTimes.get(i).start();
+      int reservedTimeEnd = reservedTimes.get(i).end();
+
+      // Add viable block if current scheduled event does not conflict
+      int potentialEnd = potentialStart + (int) duration;
+      if (potentialEnd <= reservedTimeStart) {
+        TimeRange viableTime = TimeRange.fromStartEnd(potentialStart, reservedTimeStart, false);
+        viableTimes.add(viableTime);
+      }
+
+      potentialStart = reservedTimeEnd;
+      // Guard against nested events
+      if (i < reservedTimes.size() - 1) {
+        int nextReservedTimeStart = reservedTimes.get(i+1);
+      }
     }
 
-    // Case: There are no attendees
-    if (attendees.isEmpty()) {
-      TimeRange unrestrictedDay = TimeRange.WHOLE_DAY;
-      viableTimes.add(unrestrictedDay);
-      return viableTimes;
+    int potentialEnd = potentialStart + (int) duration;
+    if (potentialEnd < TimeRange.END_OF_DAY) {
+      TimeRange viableTime = TimeRange.fromStartEnd(potentialStart, TimeRange.END_OF_DAY, true);
+      viableTimes.add(viableTime);
     }
 
     return viableTimes;
